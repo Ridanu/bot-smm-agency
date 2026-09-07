d4a87287df235548b9487cdf8bd701fbcda3d069
Ridanu <157005498+Ridanu@users.noreply.github.com>
2026-09-07 12:58:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TOKEN = os.getenv("TOKEN")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 MEMORY_FILE = "memory.json"
 
